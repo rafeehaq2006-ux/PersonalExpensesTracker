f113c1b19497da2b63f34c6a84bd32f6318411dc
Rafee <rafeehaq2006@gmail.com>
2026-06-29 12:48:35 +0100

    created models

--- MyPersonalExpensesTracker/Expensesmanagement/models.py
+++ MyPersonalExpensesTracker/Expensesmanagement/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+    
+class Expense(models.Model):
+    user = models.ForeignKey(User, ondelete=models.CASCADE, related_name='expenses')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    date = models.DateField()
+    category = models.ForeignKey('Category', ondelete=models.CASCADE, related_name='expenses')
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, ondelete=models.CASCADE, related_name='categories')
+
+    def __str__(self):
+        return self.name
 # Create your models here.
