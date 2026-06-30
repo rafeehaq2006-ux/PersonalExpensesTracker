78a00d97ad5128a24c3b505037df16d48a1e98f3
Rafee <rafeehaq2006@gmail.com>
2026-06-30 14:24:53 +0100

    improved adding expenses form

---
+++ MyPersonalExpensesTracker/Expensesmanagement/forms.py
@@ -0,0 +1,24 @@
+from .models import Expense
+from django.forms import DateInput, ModelForm, NumberInput
+from django.core.exceptions import ValidationError
+
+class ExpenseForm(ModelForm):
+    class Meta:
+        model = Expense
+        fields = '__all__'
+        widgets = {
+            'date': DateInput(attrs={'type': 'date'}),
+            'amount': NumberInput(attrs={'type': 'number', 'step': '0.01'})  
+        }
+    
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        if amount is not None and amount <= 0:
+            raise ValidationError("Amount must be a positive number.")
+        return amount
+    
+    def clean_date(self):
+        date = self.cleaned_data.get('date')
+        if date is not None and date > date.today():
+            raise ValidationError("Date cannot be in the future.")
+        return date
--- MyPersonalExpensesTracker/Expensesmanagement/views.py
+++ MyPersonalExpensesTracker/Expensesmanagement/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import redirect, render
+from django.forms import modelform_factory
+
+from Expensesmanagement.models import Expense
+from .forms import ExpenseForm
 
 def dashboard(request):
     if not request.user.is_authenticated:
@@ -8,10 +12,20 @@ def dashboard(request):
 def add_expense(request):
     if not request.user.is_authenticated:
         return redirect('login')
-    return render(request, 'Expensesmanagement/AddExpense.html')
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            expense = form.save()
+            return render(request, 'Expensesmanagement/Dashboard.html')
+        else:
+            return render(request, 'Expensesmanagement/AddExpense.html', {'form': form})
+    else:
+        form = ExpenseForm()
+    return render(request, 'Expensesmanagement/AddExpense.html', {'form': form})
 
 def view_expenses(request):
     if not request.user.is_authenticated:
         return redirect('login')
     return render(request, 'Expensesmanagement/ViewExpenses.html')
+
 # Create your views here.
