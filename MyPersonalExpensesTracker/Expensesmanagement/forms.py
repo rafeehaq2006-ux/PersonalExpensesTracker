112ea5b63841442779b0f3685391703f3e60342b
Rafee <rafeehaq2006@gmail.com>
2026-07-01 13:38:40 +0100

    added add category view

--- MyPersonalExpensesTracker/Expensesmanagement/forms.py
+++ MyPersonalExpensesTracker/Expensesmanagement/forms.py
@@ -1,4 +1,4 @@
-from .models import Expense
+from .models import Category, Expense
 from django.forms import DateInput, ModelForm, NumberInput
 from django.core.exceptions import ValidationError
 
@@ -23,3 +23,18 @@ class ExpenseForm(ModelForm):
         if date is not None and date > date.today():
             raise ValidationError("Date cannot be in the future.")
         return date
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        exclude = ['user']
+        widgets = {
+            'name': NumberInput(attrs={'type': 'text'})
+        }
+
+        def clean_name(self):
+            name = self.cleaned_data.get('name')
+            if not name:
+                raise ValidationError("Category name cannot be empty.")
+            return name
--- MyPersonalExpensesTracker/Expensesmanagement/views.py
+++ MyPersonalExpensesTracker/Expensesmanagement/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.forms import modelform_factory
 
 from Expensesmanagement.models import Expense
-from .forms import ExpenseForm
+from .forms import ExpenseForm, CategoryForm
 
 def dashboard(request):
     if not request.user.is_authenticated:
@@ -28,10 +28,23 @@ def add_expense(request):
 def view_expenses(request):
     if not request.user.is_authenticated:
         return redirect('login')
-    return render(request, 'Expensesmanagement/ViewExpenses.html')
+    return render(request, 'Expensesmanagement/ViewExpenses.html', {'expenses': Expense.objects.filter(user=request.user)})
 
 def add_category(request):
     if not request.user.is_authenticated:
         return redirect('login')
-    return render(request, 'Expensesmanagement/AddCategory.html')
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.user = request.user
+            print('USER:', request.user)
+            print('USER ID:', request.user.id)
+            category.save()
+            return render(request, 'Expensesmanagement/Dashboard.html')
+        else:
+            return render(request, 'Expensesmanagement/AddCategory.html', {'form': form})
+    else:
+        form = CategoryForm()
+    return render(request, 'Expensesmanagement/AddCategory.html', {'form': form})
 # Create your views here.
